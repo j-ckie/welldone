622315e68da38e1a08a3c287bdbdd9f6c0f3f852
Jaclyn Legacion <jglegacion@gmail.com>
2020-01-08 11:13:52 -0600

    add jsonwebtoken dependency

---
+++ util/auth.js
@@ -0,0 +1,13 @@
+const jwt = require("")
+
+module.exports = (req, res, next) => {
+    let idToken;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        idToken = req.headers.authorization.split("Bearer ")[1];
+    } else {
+        console.error("No token found");
+        return res.status(403).json({ error: "Unauthorized" });
+    }
+
+}
