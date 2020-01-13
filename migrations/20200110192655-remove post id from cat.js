d3705e87135c69226c175b98cb22b96a553b4f02
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-01-13 09:38:23 -0600

    accepted changes

---
+++ migrations/20200110192655-remove post id from cat.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Categories', 'post_id')
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+        'Categories',
+        'post_id',
+        {
+            type: Sequelize.INTEGER,
+            references: {
+                model: 'Posts',
+                key: 'id'
+            }
+        }
+    )
+  }
+};
---
+++ migrations/20200110200647-associate user has many comments.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+        'Comments',
+        'user_id',
+        {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
+        }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Comments', 'user_id')
+  }
+};
