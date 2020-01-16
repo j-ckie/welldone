05d5236ec041b25a69e1bd920a5038eb977c9a78
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-01-16 16:31:34 -0600

    hide edit post button

--- public/editpost.js
+++ public/editpost.js
@@ -1,15 +1,19 @@
 
 function showEdit(id) {
     let edit = document.getElementById(`Edit${id}`)
+    let showEditBtn = document.getElementById('show-edit-button')
     let cancelEditBtn = document.getElementById('hide-edit-button')
     cancelEditBtn.style.display = "block"
+    showEditBtn.style.display = "none"
     edit.style.display = "block"
 }
 
 function hideEdit(id) {
     let edit = document.getElementById(`Edit${id}`)
+    let showEditBtn = document.getElementById('show-edit-button')
     let cancelEditBtn = document.getElementById('hide-edit-button')
     cancelEditBtn.style.display = "none"
+    showEditBtn.style.display = "block"
     edit.style.display = "none"
 }
 
