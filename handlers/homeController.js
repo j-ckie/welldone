e575305862c1a1631a567cbcb64e193255a4fd70
Collin Barlow <Collin@Collins-MacBook-Pro.local>
2020-01-16 13:23:58 -0600

    miscellaneous changes cleaning up the site

--- handlers/homeController.js
+++ handlers/homeController.js
@@ -18,39 +18,103 @@ module.exports.getHomePage = async function(req, res) {
 //   let newpostid = postid
 //   if(newpostid = postid)
 // })})
-  let categories = await models.Categories.findAll()
 
-  let aPost = await models.Posts.findAll({
-   include:[
-     {
-     model: models.PostsWithCategories,
-    include: [
-      {
-        model: models.Categories,
-        as: "category"
+  //user
+  let user_id = await models.Users.findOne({
+    where: {
+        email: req.session.email
       }
-    ],
-      as:"postswithcategories"
-    },
-      {
-        model: models.Users,
-        as: "user"
-      } 
-]
   })
 
-  let popular = await models.Posts.findAll(
-    {
+  //categories
+  let categories = await models.Categories.findAll()
+
+  //all posts
+  let aPost = await models.Posts.findAll({
     include:[
       {
-      model: models.Notifications,   
-       as:"notification"
-     }
- ]
-}
-).then();
-  // res.json(popular);
-  // res.render('index', {categories:categories,aPost:aPost,popular:popular})  
+        model: models.PostsWithCategories,
+        include: [
+          {
+            model: models.Categories,
+            as: "category"
+          }
+        ],
+        as:"postswithcategories"
+      }, {
+        model: models.Users,
+        as: "user"
+      }, {
+        model: models.PostImage,
+        as: 'postImage'
+      }
+    ]
+  })
 
-  res.render('index', {categories: categories,aPost:aPost,popular:popular,user: req.session})
+  //adds notifications to post and cross checks the users like with the posts
+  for(let i = 0; i < aPost.length; i++){
+    let likes = await models.Notifications.findAll({
+      where: {
+        post_id: aPost[i].id,
+        type: 'like'
+      }
+    })
+
+    aPost[i].likes = likes
+
+    //finds if user liked post
+    let liked = await models.Notifications.findOne({
+      where: {
+        user_id: user_id.id,
+        type: 'like',
+        post_id: aPost[i].id
+      }
+    })
+
+    aPost[i].liked = liked
+
+    //determines which button to show
+    if(liked != null) {
+      aPost[i].hideAdd = 'hidden'
+    } else {
+      aPost[i].hideRemove = 'hidden'
+    }
+  }
+
+  //finds posts for popular
+  let popular = await models.Posts.findAll(
+    {
+      include:[
+        {
+          model: models.Notifications,
+          as:"notification",
+          where: {
+            type: 'like'
+          }
+
+        },{
+          model: models.PostImage,
+          as: 'postImage'
+        }
+      ]
+    }
+  )
+
+  //sorts posts by most likes
+  popular.sort(function(a, b) {
+    return a.notification.length - b.notification.length;
+  })
+
+  //reverses the posts to highest to lowest
+  popular.reverse()
+
+  let topten = []
+
+  //grabs the 8 most popular
+  for(let i = 0; i < 8; i++){
+    topten.push(popular[i])
+  }
+
+  //res.json(aPost);
+  res.render('index', {categories: categories, aPost:aPost, popular:topten, user: user_id})
 }
