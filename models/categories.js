3a51c586c1a3ec78030aa74f0ae9db8d4e3f5ebf
Fabian Jimenez <efjmnz@gmail.com>
2020-01-16 23:11:52 -0600

    deleted Categories belongsTo Post in categories models

--- models/categories.js
+++ models/categories.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Categories.associate = function(models) {
     models.Categories.hasMany(models.PostsWithCategories, {as: 'postswithcategories',foreignKey: 'category_id'})
-    Categories.belongsTo(models.Posts, {as: 'post',foreignKey: 'post_id'})
+    // Categories.belongsTo(models.Posts, {as: 'post',foreignKey: 'post_id'})
     // associations can be defined here
   };
   return Categories;
--- public/category.js
+++ public/category.js
@@ -1,5 +1,5 @@
-var url_string = window.location.href
-var url = new URL(url_string)
-var c = url.searchParams.get('c')
-console.log(c)
-document.getElementById('category').innerHTML = c;
+// var url_string = window.location.href
+// var url = new URL(url_string)
+// var c = url.searchParams.get('c')
+// console.log(c)
+// document.getElementById('category').innerText = c;
